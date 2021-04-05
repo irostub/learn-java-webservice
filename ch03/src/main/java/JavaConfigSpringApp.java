469038078014b356b800114e59b29b09ce3761c6
irostub <irostub@gmail.com>
2021-04-05 21:51:31 +0900

    Add main

---
+++ ch03/src/main/java/JavaConfigSpringApp.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Map;
+
+public class JavaConfigSpringApp {
+    public static void main(String[] args){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + ", value = "+beansOfType.get(key));
+        }
+    }
+}
