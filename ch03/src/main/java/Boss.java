20f64de3c204ca974d523df7589cee0d07a03818
irostub <irostub@gmail.com>
2021-04-06 22:20:30 +0900

    Add post, pre method with bean config

--- ch03/src/main/java/Boss.java
+++ ch03/src/main/java/Boss.java
@@ -1,9 +1,14 @@
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Boss implements WorkManager{
 
+    @PostConstruct
     public void onCreate() {
         System.out.println("Boss.onCreate");
     }
 
+    @PreDestroy
     public void onDestroy() {
         System.out.println("Boss.onDestroy");
     }
--- ch03/src/main/java/Employee.java
+++ ch03/src/main/java/Employee.java
@@ -1,9 +1,14 @@
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Employee implements WorkManager{
 
+    @PostConstruct
     public void onCreate() {
         System.out.println("Employee.onCreate");
     }
 
+    @PreDestroy
     public void onDestroy() {
         System.out.println("Employee.onDestroy");
     }
